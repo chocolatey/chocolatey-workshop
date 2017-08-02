bee2152449fc584c49c5ac0898c2152ed614c6d8
Rob Reynolds <ferventcoder@gmail.com>
2017-08-02 16:22:43 -0500

    (doc) add solution for Exercise 2

---
+++ solutions/Exercise2/googlechrome/tools/_ReadMe.md
@@ -0,0 +1,2 @@
+Please add googlechromestandaloneenterprise64.msi here in this directory.
+Delete this file after you do.
---
+++ solutions/Exercise2/googlechrome/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'googlechromestandaloneenterprise64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  file         = $fileLocation
+  softwareName  = 'Google Chrome*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs 
