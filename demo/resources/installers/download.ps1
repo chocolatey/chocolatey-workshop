b0855abc8bcf76252c982e5e55d15c70a4067ebf
Rob Reynolds <ferventcoder@gmail.com>
2018-03-07 15:18:10 -0600

    fix download script

--- demo/resources/installers/download.ps1
+++ demo/resources/installers/download.ps1
@@ -5,9 +5,9 @@
 & curl.exe -O https://d13itkw33a7sus.cloudfront.net/dist/1P/win4/1Password-4.6.0.598.exe
 & curl.exe -O https://d13itkw33a7sus.cloudfront.net/dist/1P/win4/1Password-4.6.1.617.exe
 
-& curl.exe -O http://www.7-zip.org/a/7z1801-x64.exe
-& curl.exe -O http://www.7-zip.org/a/7z1801-x64.msi
+& curl.exe -L -O http://www.7-zip.org/a/7z1801-x64.exe
+& curl.exe -L -O http://www.7-zip.org/a/7z1801-x64.msi
 # get from main machine https://www.charlesproxy.com/download/
 # get from main machine http://www.7-zip.org/download.html
 
-choco download baretail chocolatey chocolatey-agent chocolatey.extension chocolateygui chocolatey.server   dotnet4.5.2 dotnet4.6.1 git launchy notepadplusplus visualstudiocode virtualbox --internalize  --internalize-all-urls --append-use-original-location --output-directory="'..\demo\packages'"
+choco download baretail chocolatey chocolatey-agent chocolatey.extension chocolateygui chocolatey.server   dotnet4.5.2 dotnet4.6.1 git launchy notepadplusplus visualstudiocode virtualbox --force --internalize  --internalize-all-urls --append-use-original-location --output-directory=..\..\packages
