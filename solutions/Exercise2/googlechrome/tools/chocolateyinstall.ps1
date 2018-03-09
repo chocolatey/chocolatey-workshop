73bb61bce2cfefb86e2c4cb868edd720cc1298c5
Rob Reynolds <ferventcoder@gmail.com>
2018-03-08 16:33:04 -0800

    (doc) clean up formatting

--- solutions/Exercise2/googlechrome/tools/chocolateyinstall.ps1
+++ solutions/Exercise2/googlechrome/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $fileLocation = Join-Path $toolsDir 'googlechromestandaloneenterprise64.msi'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
-  file         = $fileLocation
-  softwareName  = 'Google Chrome*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  file          = $fileLocation
+  softwareName  = 'Google Chrome*'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyInstallPackage @packageArgs 
+Install-ChocolateyInstallPackage @packageArgs
